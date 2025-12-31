05a14d84dab5e8cc54a15b1cdcc8c68d9308fda9
Monique K <moniqueklingenberg.mk@gmail.com>
2025-12-31 07:13:08 +0200

    Add scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,321 @@
+"""Basic CSP scheduler for internal medicine doctors using backtracking search"""
 
-def scheduler():
-    print("Performing scheduler tasks...")
-    return
+from collections import defaultdict
+import datetime
+from pathlib import Path
+import random
+
+from models.shift_calendar import ShiftCalendar
+from services.load_inputs import (
+    load_doctors,
+    load_shift_structure,
+    load_public_holidays,
+)
+from utility.calendar_utils import DayType
+
+def generate_schedule(
+    start_date: datetime.date,
+    end_date: datetime.date,
+    doctors_path: Path,
+    leave_path: Path,
+    pairing_constraints_path: Path,
+    shift_structure_path: Path,
+    public_holidays_path: Path,
+):
+    """Generates a schedule using CSP backtracking search"""
+    #1 load doctors ("variables")
+    int_doctors = load_doctors(
+        doctors_path=doctors_path,
+        leave_path=leave_path,
+        preferences_path=None, # preferences not used in this version
+        pairing_constraints_path=pairing_constraints_path,
+    )
+
+    doctors = list(int_doctors.keys())
+
+    #2.1 get shift slots ("nodes")
+    shift_structure = load_shift_structure(shift_structure_path, 0)
+    public_holidays = load_public_holidays(public_holidays_path)
+    day_type = DayType(public_holidays)
+
+    shift_calendar = ShiftCalendar(start_date, end_date, shift_structure, day_type)
+
+    #2.2 check if day is counted as a weekend shift
+    def is_weekend_shift(shift):
+        day_info = shift_calendar.calendar[shift.date]
+        return day_info["day_type"] in ("friday", "saturday", "sunday")
+
+    shift_slots = []
+    shift_by_id = {}
+
+    for __, day_info in shift_calendar.calendar.items():
+        for shift in day_info["shifts"]:
+            shift_slots.append(shift.shift_id)
+            shift_by_id[shift.shift_id] = shift
+
+    #Sort shift by date - normal sort as ymd format is already "alphabetical"
+    shift_slots.sort()
+
+    #3. Generate initial domains for each shift slot
+    domains = {}
+    for shift_id in shift_slots:
+        domains[shift_id] = doctors.copy()
+
+    #3.1 Unary constraints
+    #3.1a Unary constraints: leave
+    def apply_leave_filter(shift_id, domain, int_doctors, shift_by_id):
+        """Returns a list of doctors not on leave for the given shift"""
+        shift = shift_by_id[shift_id]
+        shift_date = shift.date
+
+        valid_docs = []
+        for doc in domain:
+            doctor = int_doctors[doc]
+            if shift_date not in doctor.leave_dates:
+                valid_docs.append(doc)
+
+        return valid_docs
+
+    #3.1b Unary constraint: doctors requiring overlap shifts cannot work non-overlap shifts
+    def apply_overlap_filter(shift_id, domain, int_doctors, shift_by_id):
+        """Filters domains based on doctors requiring overlap"""
+        shift = shift_by_id[shift_id]
+        valid_docs = []
+
+        for doc in domain:
+            doctor = int_doctors[doc]
+
+            if shift.overlap:
+                valid_docs.append(doc)
+
+            else:
+                if not doctor.requires_overlap:
+                    valid_docs.append(doc)
+
+        return valid_docs
+
+    #3.2 Filter by unary constraints
+    for shift_id in shift_slots:
+        domains[shift_id] = apply_leave_filter(
+            shift_id,
+            domains[shift_id],
+            int_doctors,
+            shift_by_id
+        )
+
+    for shift_id in shift_slots:
+        domains[shift_id] = apply_overlap_filter(
+            shift_id,
+            domains[shift_id],
+            int_doctors,
+            shift_by_id
+        )
+
+    #4. CSP solver framework
+
+    assignment = {}
+
+    doc_hours = {doc: 0 for doc in doctors} #to track number of hours per doctor
+    doc_weekend_hours = {doc: 0 for doc in doctors} #to track number of weekend hours per doc
+
+    #4.1 Variable Selection: Minimum Remaining Values (MRV)
+    def select_unassigned_variable(assignment, domains):
+        """Selects the unassigned variable with fewest remaining values in its domain."""
+        unassigned = [var for var in domains if var not in assignment]
+        mrv_var = min(unassigned, key=lambda var: len(domains[var]))
+
+        #checks all mrv domains and selects randomly among ties
+        candidates = [var for var in unassigned if len(domains[var]) == len(domains[mrv_var])]
+        return random.choice(candidates)
+
+    #4.2 Value Ordering: Least Constraining Value (LCV)
+    def order_domain_values(shift_id, domains, doc_hours, doc_weekend_hours, shift_by_id):
+        """Orders doctors for a shift based on least assigned hours (LCV)"""
+        if not domains[shift_id]:
+            return []
+
+        shift = shift_by_id[shift_id]
+        is_weekend = is_weekend_shift(shift) #this would include Friday-Sunday and PH
+
+        #sort by total assigned hours
+        min_doc = min(domains[shift_id], key=lambda doc: doc_hours[doc])
+        lowest_hours = doc_hours[min_doc]
+
+        candidates = [doc for doc in domains[shift_id] if doc_hours[doc] == lowest_hours]
+
+        if not is_weekend:
+            random.shuffle(candidates)
+            return candidates
+
+        else:
+            min_weekend_doc = min(candidates, key=lambda doc: doc_weekend_hours[doc])
+            lowest_weekend_hours = doc_weekend_hours[min_weekend_doc]
+
+            weekend_candidates = [doc for doc in candidates if doc_weekend_hours[doc] ==
+                                lowest_weekend_hours]
+            random.shuffle(weekend_candidates)
+            return weekend_candidates
+
+    #5. Consistency Checking Functions
+    #5.1 No doctor two shifts in one day
+    def violates_same_day(shift_id, doc, assignment, shift_by_id):
+        """Checks if assigning doctor to shift violates same day constraint
+        Returns True if violation occurs, False otherwise"""
+        shift_date = shift_by_id[shift_id].date
+
+        for assigned_shift_id, assigned_doc in assignment.items():
+            if assigned_doc != doc:
+                continue
+
+            assigned_date = shift_by_id[assigned_shift_id].date
+            if assigned_date == shift_date:
+                return True
+
+        return False
+
+    #5.2 No doctor two days in a row
+    def violates_consecutive_days(shift_id, doc, assignment, shift_by_id):
+        """Checks if assigning doctor to shift violates consecutive days constraint
+        Returns True if violation occurs, False otherwise"""
+        shift_date = shift_by_id[shift_id].date
+
+        for assigned_shift_id, assigned_doc in assignment.items():
+            if assigned_doc != doc:
+                continue
+
+            assigned_date = shift_by_id[assigned_shift_id].date
+            if abs((assigned_date - shift_date).days) == 1: #abs value of timedelta 1 is consec.
+                return True
+
+        return False
+
+    #5.3 D and E cannot work together
+    def violates_avoid_pair(shift_id, doc, assignment, shift_by_id, int_doctors):
+        """Checks if assigning doctor to shift violates avoid pair constraint
+        Returns True if violation occurs, False otherwise"""
+        shift_date = shift_by_id[shift_id].date
+        doc_obj = int_doctors[doc]
+
+        for assigned_shift_id, assigned_doc in assignment.items():
+            assigned_date = shift_by_id[assigned_shift_id].date
+            if assigned_date != shift_date:
+                continue
+
+            if assigned_doc in doc_obj.avoid_pair:
+                return True
+
+            if doc in int_doctors[assigned_doc].avoid_pair:
+                return True
+
+        return False
+
+    #5.4 G requires a pair from A-D
+    def violates_requires_pair(shift_id, doc, assignment, shift_by_id, int_doctors):
+        """Checks if assigning doctor to shift violates requires pair constraint
+        Returns True if violation occurs, False otherwise"""
+        doc_obj = int_doctors[doc]
+
+        if not doc_obj.requires_pair:
+            return False
+
+        shift_date = shift_by_id[shift_id].date
+        required_partners = set(doc_obj.requires_pair)
+
+        partner_found = False
+        unassigned_shift_exists = False
+
+        for other_shift_id, other_shift in shift_by_id.items():
+            if other_shift.date != shift_date:
+                continue
+
+            if other_shift_id in assignment:
+                assigned_doc = assignment[other_shift_id]
+                if assigned_doc in required_partners:
+                    partner_found = True
+            else:
+                unassigned_shift_exists = True
+
+        if partner_found:
+            return False
+
+        if unassigned_shift_exists:
+            return False
+
+        return True
+
+    #6. Check consistency of assignment
+    def is_consistent(shift_id, doc, assignment, shift_by_id, int_doctors):
+        """Checks if assigning doctor to shift is consistent with all constraints"""
+        if violates_same_day(shift_id, doc, assignment, shift_by_id):
+            return False
+
+        if violates_consecutive_days(shift_id, doc, assignment, shift_by_id):
+            return False
+
+        if violates_avoid_pair(shift_id, doc, assignment, shift_by_id, int_doctors):
+            return False
+
+        if violates_requires_pair(shift_id, doc, assignment, shift_by_id, int_doctors):
+            return False
+
+        return True
+
+    #7. Backtracking Search Algorithm
+    def backtrack(assignment, domains):
+        """Backtracking search algorithm to find a valid assignment"""
+        if len(assignment) == len(domains):
+            return assignment
+
+        #Get the next shift with the MRV function
+        shift_id = select_unassigned_variable(assignment, domains)
+
+        #Try filling it with the LCV ordered doctors
+        for doc in order_domain_values(
+                    shift_id,
+                    domains,
+                    doc_hours,
+                    doc_weekend_hours,
+                    shift_by_id
+                    ):
+            if is_consistent(shift_id, doc, assignment, shift_by_id, int_doctors):
+                assignment[shift_id] = doc
+                doc_hours[doc] += shift_by_id[shift_id].hours
+
+                if is_weekend_shift(shift_by_id[shift_id]):
+                    doc_weekend_hours[doc] += shift_by_id[shift_id].hours
+
+                result = backtrack(assignment, domains)
+                if result is not None:
+                    return result
+
+                #Backtrack
+                del assignment[shift_id]
+                doc_hours[doc] -= shift_by_id[shift_id].hours
+
+                if is_weekend_shift(shift_by_id[shift_id]):
+                    doc_weekend_hours[doc] -= shift_by_id[shift_id].hours
+
+        return None
+
+    solution = backtrack(assignment, domains)
+
+    if solution is None:
+        raise RuntimeError("No valid schedule found")
+
+    #8. Iniitialise metadata tracking for doctors
+
+    doctor_stats = defaultdict(lambda: {
+        "shifts": 0,
+        "weekend_shifts": 0,
+        "hours": 0,
+    })
+
+    for shift_id, doc in solution.items():
+        shift = shift_by_id[shift_id]
+        doctor_stats[doc]["shifts"] += 1
+        doctor_stats[doc]["hours"] += shift.hours
+
+        if is_weekend_shift(shift):
+            doctor_stats[doc]["weekend_shifts"] += 1
+
+    return (solution, shift_by_id, doctor_stats)
